0b3ab281f214e27e86062a1970e8982dd83cbdcc
noahyoungs <nly253@icloud.com>
2020-12-09 02:08:59 -0700

    Added front end interface for user preferences and search, added preferences to user table in database

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -10,10 +10,19 @@ CREATE TABLE Messages
 
 CREATE TABLE Users
 (
-    id       INTEGER PRIMARY KEY,
-    email    STRING UNIQUE,
-    username STRING,
-    password STRING
+    id            INTEGER PRIMARY KEY,
+    email         STRING UNIQUE,
+    username      STRING,
+    password      STRING,
+    fastFoodIndex FLOAT DEFAULT 0.5,
+    priceIndex    FLOAT DEFAULT 0.5,
+    americanIndex FLOAT DEFAULT 0.5,
+    mexicanIndex  FLOAT DEFAULT 0.5,
+    chineseIndex  FLOAT DEFAULT 0.5,
+    pizzaIndex    FLOAT DEFAULT 0.5,
+    alcoholIndex  FLOAT DEFAULT 0.5,
+    musicIndex    FLOAT DEFAULT 0.5,
+
 );
 
 CREATE TABLE AuthTokens
